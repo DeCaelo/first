c070411647842a98231a3b9ca52f256f2c21e5c4
DeCaelo <ludovic.cleuet@gmail.com>
2023-10-10 21:59:51 +0200

    templating Strings with the format! macro

--- readme.md
+++ readme.md
@@ -9,3 +9,8 @@ rustc --explain E0308
 ```
 cargo run one something 42 "with spaces"
 ```
+
+```
+cargo run post rust,bevy "Doing shader stuff in Bevy" doing-shader-stuff-in-be
+vy.md
+```
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,19 @@ fn main() {
     let layout = &args[1];
     let tags = &args[2];
     let heading = &args[3];
-    let filename = &args[4];
+    let _filename = &args[4];
 
-    dbg!(layout, tags, heading, filename);
+    let new_file_contents = format!(
+        "---
+layout: {layout}
+tags: {tags}
+status: draft
+---
+
+# {heading}
+
+"
+    );
+
+    dbg!(new_file_contents);
 }
