8a9e3be6001ee74ac8662fb4ff988ccbd4af0cd6
DeCaelo <ludovic.cleuet@gmail.com>
2023-10-10 21:53:21 +0200

    borrowing

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    dbg!(&args);
+
+    let layout = &args[1];
+    let tags = &args[2];
+    let heading = &args[3];
+    let filename = &args[4];
+
+    dbg!(layout, tags, heading, filename);
 }
